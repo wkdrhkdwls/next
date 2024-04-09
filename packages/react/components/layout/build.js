71d050f9f7c661908cec72b4f34c3797984a2e09
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-04-09 18:34:42 +0900

    Feat : 기본세팅

--- packages/react/components/layout/build.js
+++ packages/react/components/layout/build.js
@@ -2,7 +2,9 @@ import run from "@custom/esbuild-config";
 import pkg from "./package.json" assert { type: "json" };
 import { vanillaExtractPlugin } from "@vanilla-extract/esbuild-plugin";
 
-const config = { plugins: [vanillaExtractPlugin()] };
+const config = {
+  plugins: [vanillaExtractPlugin()],
+};
 
 run({
   pkg,
