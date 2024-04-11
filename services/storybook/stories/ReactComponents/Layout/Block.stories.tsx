36d413ae46b41b2a6a1dc8ee256d09f1ac40b617
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-04-11 18:56:20 +0900

    Feat : 스토리북 setting

---
+++ services/storybook/stories/ReactComponents/Layout/Block.stories.tsx
@@ -0,0 +1,13 @@
+import { Block as _Block } from "@custom/react-components-layout";
+import "@custom/react-components-layout/style.css";
+
+export default {
+  title: "React Components/Layout/Block",
+  component: _Block,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const BlockStory = {};
